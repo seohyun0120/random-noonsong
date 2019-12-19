7e266e578c9c982811002596c3d041df114912ae
Seohyun Yoon <sarahseohyun@gmail.com>
2019-12-20 01:48:24 +0900

    햄버거 메뉴 구현

---
+++ src/components/Burger.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+import React from 'react'
+
+interface IBurgerProps {
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
+export default function Burger(props: IBurgerProps) {
+  const isExpanded = open ? true : false;
+
+  return (
+    <StyledBurger
+      aria-label='Toggle menu'
+      aria-expanded={isExpanded}
+      open={props.open}
+      onClick={() => props.setOpen(!props.open)}
+    >
+      <span />
+      <span />
+      <span />
+    </StyledBurger>
+  )
+}
+
+interface IButtonProps {
+  open: boolean
+}
+
+const StyledBurger = styled.button<IButtonProps>`
+  position: absolute;
+  top: 5%;
+  left: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+  span {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ theme, open }) => open ? theme.primaryWhite : theme.primaryBlack};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+    :first-child {
+      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? '0' : '1')};
+      transform: ${({ open }) => (open ? 'translateX(20px)' : 'translateX(0)')};
+    }
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`
+
---
+++ src/components/Menu.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+interface IMenuProps {
+  id: string
+  open: boolean
+  setOpen: (open: boolean) => void
+}
+
+export default function Menu(props: IMenuProps) {
+  const isHidden = props.open ? true : false;
+  const tabIndex = isHidden ? 0 : -1;
+
+  return (
+    <StyledMenu
+      open={props.open}
+      aria-hidden={!isHidden}
+      {...props}
+    >
+      <Link
+        to='/'
+        tabIndex={tabIndex}
+        onClick={() => props.setOpen(!props.open)} style={{ textDecoration: 'none', outline: 0 }}
+      >
+        <MenuText>랜덤눈송이</MenuText>
+      </Link>
+      <Link
+        to='/about'
+        tabIndex={tabIndex}
+        onClick={() => props.setOpen(!props.open)} style={{ textDecoration: 'none', outline: 0 }}
+      >
+        <MenuText>About</MenuText>
+      </Link>
+    </StyledMenu >
+  )
+}
+
+export const StyledMenu = styled.div<IMenuProps>`
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.primaryBlack};
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  width: 18rem;
+  height: 100%;
+  position: absolute;
+  text-align: left;
+  padding: 5.5rem 2rem 2rem 2rem;
+  top: 0;
+  left: 0;
+  transition: transform 0.3s ease-in-out;
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 80%;
+  }
+`
+
+const MenuText = styled.div`
+  font-size: 2.25rem;
+  padding: 1rem 0;
+  font-weight: bold;
+  color: ${({ theme }) => theme.primaryWhite};
+  text-decoration: none;
+  transition: color 0.3s linear;
+  &:hover {
+    color: ${({ theme }) => theme.primaryBlue};
+  }
+`
