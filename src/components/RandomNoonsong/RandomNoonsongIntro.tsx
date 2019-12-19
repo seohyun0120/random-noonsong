67c04443513f90b493f3cb6574a1b57cca8ad388
Seohyun Yoon <sarahseohyun@gmail.com>
2019-12-20 03:50:16 +0900

    Functional Component로 변경

--- src/components/RandomNoonsong/RandomNoonsongIntro.tsx
+++ src/components/RandomNoonsong/RandomNoonsongIntro.tsx
@@ -1,31 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default class Intro extends React.Component {
-  render() {
-    return (
-      <IntroBox>
-        <Title> Random noonsong </Title>
-        <Description>나만의 눈송이를 만들어보세요.</Description>
-        <Description>닉네임은 최대 10글자까지 입력할 수 있어요.</Description>
-        <Description>이모티콘도 하나 골라보세요.</Description>
-      </IntroBox>
-    )
-  }
+export default function Intro() {
+  return (
+    <Container>
+      <Title> Random Noonsong </Title>
+      <Description>나만의 눈송이</Description>
+      <Description>닉네임을 입력하고 나만의 이모티콘을 골라보세요</Description>
+    </Container>
+  )
 }
 
-const IntroBox = styled.div`
-  display: block;
+const Container = styled.div`
+  position: absolute;
+  top: 12.5%;
+  left: 2.5rem;
+  display: flex;
   flex-direction: column;
+  justify-content: space-around;
+  padding: 0;
 `
 
 const Title = styled.div`
-  font-size: 26px;
+  font-size: 3.125rem;
   font-weight: 700;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 26px;
+  }
 `
 
 const Description = styled.div`
-  font-size: 13px;
+  font-size: 1.225rem;
   font-weight: 500;
   padding-top: .25rem;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 15px;
+  }
 `
