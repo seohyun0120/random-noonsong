0943b45e3b43c0c282daf5c5aac59087fae4ad98
Seohyun Yoon <sarahseohyun@gmail.com>
2019-12-25 05:34:32 +0900

    색상 추가

--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,9 @@ import baseStyled, { ThemedStyledInterface } from 'styled-components'
 export const theme = {
   primaryWhite: '#FFFFFF',
   primaryBlack: '#000000',
-  primaryBlue: '#74c0fc',
   primaryOrange: '#fd7e14',
+  primaryBlue: '#74c0fc',
+  primaryGreen: '#40c057',
   primaryGray: '#343a40',
   mobile: '576px'
 }
