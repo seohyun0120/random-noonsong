a8060b1bc8522a3e8b097754d4450e7c57dec424
Seohyun Yoon <sarahseohyun@gmail.com>
2020-01-13 00:38:55 +0900

    change color theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,11 +3,13 @@ import baseStyled, { ThemedStyledInterface } from 'styled-components'
 export const theme = {
   nsLabWhite: '#FFFFFF',
   nsLabBlack: '#000000',
-  nsLabGray: '#757575',
   nsLabGold: '#84754e',
   nsLabBlue: '#003087',
   nsLabSkyBlue: '#0072ce',
-  mobile: '980px'
+  nsLabYellow: '#fcc419',
+  nsLabRed: '#f55950',
+  nsLabGray: '#f5f5f5',
+  mobile: '1024px'
 }
 
 export type Theme = typeof theme;
