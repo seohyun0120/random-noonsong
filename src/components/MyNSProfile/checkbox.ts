fc76efa4e9134ac0ca7c00efc8c8b27137f357f6
Seohyun Yoon <sarahseohyun@gmail.com>
2020-02-02 17:11:27 +0900

    checkbox lists

---
+++ src/components/MyNSProfile/checkbox.ts
@@ -0,0 +1,93 @@
+const function2Checkbox = [
+  {
+    id: 1,
+    name: '데자와'
+  },
+  {
+    id: 2,
+    name: '마돈나'
+  },
+  {
+    id: 3,
+    name: '로즈커피'
+  },
+  {
+    id: 4,
+    name: '본솔'
+  },
+  {
+    id: 5,
+    name: '스타벅스'
+  }
+]
+
+const function3Checkbox = [
+  {
+    id: 1,
+    name: '초롱초롱 +_+'
+  },
+  {
+    id: 2,
+    name: '교수님.. 이해가 안가요... X_X'
+  },
+  {
+    id: 3,
+    name: '잠은 수업 시간에! zzZZ'
+  },
+  {
+    id: 4,
+    name: '모두 다 이해한 척 0_0'
+  }
+]
+
+const function4Checkbox = [
+  {
+    id: 1,
+    name: '노트북'
+  },
+  {
+    id: 2,
+    name: '아이패드 + 애플펜슬'
+  },
+  {
+    id: 3,
+    name: '아이패드 + 키보드'
+  },
+  {
+    id: 4,
+    name: '프린트에 직접 필기하기'
+  },
+  {
+    id: 5,
+    name: '필기는 거들뿐...'
+  }
+]
+
+const function5Checkbox = [
+  {
+    id: 1,
+    name: '신선함 Green'
+  },
+  {
+    id: 2,
+    name: '차가움 Blue'
+  },
+  {
+    id: 3,
+    name: '하얀 White'
+  },
+  {
+    id: 4,
+    name: '따뜻함 Red'
+  },
+  {
+    id: 5,
+    name: '발랄함 Yellow'
+  }
+]
+export {
+  function2Checkbox,
+  function3Checkbox,
+  function4Checkbox,
+  function5Checkbox
+}
